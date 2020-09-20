b3dfef82835f7e493c4e3b46bdcb33842455f7fa
Iino Ten <nteten0313@icloud.com>
2020-09-21 03:16:40 +0900

    RT数で絞りこんだ

--- main.py
+++ main.py
@@ -20,10 +20,10 @@ twitter = OAuth1Session(CK, CS, AT, ATS)
 url = 'https://api.twitter.com/1.1/search/tweets.json'
 
 # Enedpointへ渡すパラメーター
-keyword = ["マスク"]
+keyword = ["コロナ"]
 
 params ={
-         'count' : 5,      # 取得するtweet数
+         'count' : 100,      # 取得するtweet数
          'q'     :  keyword# 検索キーワード
          }
 
@@ -32,16 +32,8 @@ req = twitter.get(url, params = params)
 if req.status_code == 200:
     res = json.loads(req.text)
     for line in res['statuses']:
-        test = "honyahonya"
-        tweet_degree_point = 100
-        if ( not bool( line['user']['verified'] ) ) : tweet_degree_point -= 10      #公認バッジ付きのアカウントでないなら-10ポイント
-        if  line['user']['statuses_count'] < 40000  : tweet_degree_point -= 10#総ツイート数が4万以内なら-10ポイント
-        if  line['user']['default_profile'] : tweet_degree_point -= 10              #プロフィール文が初期のままなら-10ポイント
-        if  line['user']['default_profile_image']  : tweet_degree_point -= 10    #アイコンが初期のままなら-10ポイント
-        if  line['entities']['urls']  : tweet_degree_point -= 10                      #参考URLとかがなかったら-10ポイント
-
-        print( "https://twitter.com/" + line['user']['screen_name'] + "/status/" + line['id_str'] ) #ツイートのURLを表示
-        print( tweet_degree_point ) #評価の点数を表示
-        print('*******************************************')
+        if line["retweet_count"] >500: #RT数が500以上のツイートを表示 
+            print( "https://twitter.com/" + line['user']['screen_name'] + "/status/" + line['id_str'] ) #ツイートのURLを表示
+            print('*******************************************')
 else:
     print("Failed: %d" % req.status_code)
