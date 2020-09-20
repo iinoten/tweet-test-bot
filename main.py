21afaf81bdc947032664a3633cfebb5bb5c278d3
Iino Ten <nteten0313@icloud.com>
2020-09-21 04:57:52 +0900

    RT数20以上、フォロワー数500以下のツイートを取得

--- main.py
+++ main.py
@@ -17,23 +17,30 @@ ATS = os.environ['ATS']
 twitter = OAuth1Session(CK, CS, AT, ATS)
 
 # Twitter Endpoint(検索結果を取得する)
-url = 'https://api.twitter.com/1.1/search/tweets.json'
+timeline_url = 'https://api.twitter.com/1.1/search/tweets.json'
+
+#Twitter フォロワー数を取得する エンドポイント
+follower_url = 'https://api.twitter.com/1.1/followers/ids.json'
 
 # Enedpointへ渡すパラメーター
-keyword = ["コロナ"]
+keyword = ["マスク min_retweets:20"]#20RT以上されてるツイートを取得
 
 params ={
-         'count' : 100,      # 取得するtweet数
-         'q'     :  keyword# 検索キーワード
+         'count' : 1000,      # 取得するtweet数
+         'q'     :  keyword,# 検索キーワード
+         'result_type': 'mixed',#時系列で取得
+         'exclude': 'retweets'#RTされて表示されているツイートを除外
          }
 
-req = twitter.get(url, params = params)
+timeline_req = twitter.get(timeline_url, params = params)
+followers_req = twitter.get(follower_url, params = {'screen_name': '@iinoten'})
 
-if req.status_code == 200:
-    res = json.loads(req.text)
+if timeline_req.status_code == 200:
+    res = json.loads(timeline_req.text)
     for line in res['statuses']:
-        if line["retweet_count"] >500: #RT数が500以上のツイートを表示 
+        if ( line["user"]["followers_count"] <= 500 ): #フォロワー数が500以下のツイートを表示 
+            print( line["text"] )
             print( "https://twitter.com/" + line['user']['screen_name'] + "/status/" + line['id_str'] ) #ツイートのURLを表示
             print('*******************************************')
 else:
-    print("Failed: %d" % req.status_code)
+    print("Failed: %d" % timeline_req.status_code)
