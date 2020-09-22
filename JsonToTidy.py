d9a5cee338735ae228b69ccd17bcd14f997a8467
Iino Ten <nteten0313@icloud.com>
2020-09-22 20:19:49 +0900

    意味の無い文言のみの文と空白のみの行を削除

--- JsonToTidy.py
+++ JsonToTidy.py
@@ -2,5 +2,13 @@ import json
 
 json_open = open('./netnewsdata.json', 'r')
 json_load = json.load(json_open)
+news_data_array = json_load['data']
 
-print(json_load['data'][0])
+form_news_data_array = []
+for index, news_item in enumerate(news_data_array):
+  replaced_news_text = news_item.replace("\u3000", "")
+  replaced_news_text = news_item.replace("None", "")
+  if not(replaced_news_text==''): form_news_data_array.append(replaced_news_text)
+
+with open('./replaced_netnewsdata.json', 'w') as f:
+    json.dump({"data":form_news_data_array}, f, ensure_ascii=False, indent=4)
