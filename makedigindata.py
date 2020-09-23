1b0e8e1eacf95b2b516770a2a149d31419bdebe2
Iino Ten <nteten0313@icloud.com>
2020-09-23 16:23:33 +0900

    配列回してグラフを作成

---
+++ makedigindata.py
@@ -0,0 +1,19 @@
+import matplotlib.pyplot as plt
+import json
+ 
+json_open = open('./analysed_netnewsdata.json', 'r')
+json_load = json.load(json_open)
+analysed_news_data_array = json_load['analysed_data']
+ 
+# figureを生成する
+fig = plt.figure()
+ 
+# axをfigureに設定する
+ax = fig.add_subplot(1, 1, 1)
+ 
+# axesに散布図を設定する
+for index, analysed_result in enumerate(analysed_news_data_array):
+  ax.scatter(x=analysed_result["documentSentiment"]["magnitude"], y=analysed_result["documentSentiment"]["score"], c='b', s=1)
+ 
+# 表示する
+plt.savefig("dig_in_scalar_plot_graph.jpg")
