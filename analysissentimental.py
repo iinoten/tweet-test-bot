167f1c90b28793b399243a8e321ecb5cd5968813
Iino Ten <nteten0313@icloud.com>
2020-09-27 04:09:16 +0900

    引数のテキストを分析して返す

--- analysissentimental.py
+++ analysissentimental.py
@@ -16,27 +16,18 @@ key = GOOGLE_LANG_API_KEY
 #APIのURL
 url = 'https://language.googleapis.com/v1/documents:analyzeSentiment?key=' + key
 
-analysed_data_json = []
 
-for index, news_text in enumerate(news_data_array):
-  #基本情報の設定 JAをENにすれば英語のテキストを解析可能
-  header = {'Content-Type': 'application/json'}
-  body = {
-      "document": {
-          "type": "PLAIN_TEXT",
-          "language": "JA",
-          "content": news_text
-      },
-      "encodingType": "UTF8"
-  }
-  #json形式で結果を受け取る。
-  response = requests.post(url, headers=header, json=body).json()
-  #分析の結果をコンソール画面で見やすく表示
-  print(response["documentSentiment"])
-  analysed_data_json.append({
-    "text": news_text,
-    "documentSentiment": response["documentSentiment"]
-  })
-print (analysed_data_json)
-with open('./analysed_netnewsdata.json', 'w') as f:
-    json.dump({"analysed_data":analysed_data_json}, f, ensure_ascii=False, indent=4)
+#基本情報の設定 JAをENにすれば英語のテキストを解析可能
+header = {'Content-Type': 'application/json'}
+body = {
+    "document": {
+        "type": "PLAIN_TEXT",
+        "language": "JA",
+        "content": "おはようございみゃす☁左ひざを痛めみゃした…曲げると痛い…。特別に何かした記憶もなく、風呂上がりに感じる痛み…。ぴえん訴えかける顔さて、今日は月末最後の金曜日「プレミアム文房具デー！」お気に入りの文房具を見つけに、あのお店に行こう！"
+    },
+    "encodingType": "UTF8"
+}
+#json形式で結果を受け取る。
+response = requests.post(url, headers=header, json=body).json()
+#分析の結果をコンソール画面で見やすく表示
+print(response)
