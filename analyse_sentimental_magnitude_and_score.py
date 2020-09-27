f92dc447b35177a01dabe4dacd92a61fb6ebbdf9
Iino Ten <nteten0313@icloud.com>
2020-09-27 18:35:18 +0900

    テキストを解析して感情値の合計を変動を取得する処理を実装

--- analyse_sentimental_magnitude_and_score.py
+++ analyse_sentimental_magnitude_and_score.py
@@ -1,3 +1,40 @@
-def hello():
-  print("hello")
-  return
+import requests 
+import os
+from dotenv import load_dotenv
+import math
+
+def load_sentimental(text):
+  load_dotenv()
+  GOOGLE_LANG_API_KEY  = os.environ['GOOGLE_LANG_API_KEY']
+  #APIキーを入力
+  key = GOOGLE_LANG_API_KEY
+
+  #APIのURL
+  url = 'https://language.googleapis.com/v1/documents:analyzeSentiment?key=' + key
+
+
+  #基本情報の設定 JAをENにすれば英語のテキストを解析可能
+  header = {'Content-Type': 'application/json'}
+  body = {
+      "document": {
+          "type": "PLAIN_TEXT",
+          "language": "JA",
+          "content": text
+      },
+      "encodingType": "UTF8"
+  }
+  #json形式で結果を受け取る。
+  response = requests.post(url, headers=header, json=body).json()
+  #感情スコアの変動のための変数を初期化
+  variable_sepalate_sentimental = 0
+  #前回に処理しておいた感情スコアを保持しておくための変数を初期化
+  hold_senti_score = 0
+  for sentence_result in response["sentences"]:
+    sepalate_score_point = sentence_result["sentiment"]["score"]
+    variable_sepalate_sentimental += math.fabs( hold_senti_score - sepalate_score_point )
+    #保持するためのスコアに今回のスコアを保存
+    hold_senti_score = sepalate_score_point
+  return({
+    "magnitude": response["documentSentiment"]["magnitude"],
+    "sentences_fluctuation": variable_sepalate_sentimental
+  })
