4ae35cd414c7cb1d3bf7d8010a2cf9614c4c3796
ayamega <58317172+ayamega@users.noreply.github.com>
2020-09-28 18:24:36 +0900

    勢いのあるツイートを取得し、リプライを送る機能を追加

--- main.py
+++ main.py
@@ -1,33 +1,35 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+import datetime
 import json
 from requests_oauthlib import OAuth1Session
 
+now_time = datetime.datetime.now()
+
 CK  = 
 CS  = 
-AT  =
+AT  = 
 ATS = 
 twitter = OAuth1Session(CK, CS, AT, ATS)
 # Twitter Endpoint(検索結果を取得する)
 timeline_url = 'https://api.twitter.com/1.1/search/tweets.json'
-#Twitter フォロワー数を取得する エンドポイント
-follower_url = 'https://api.twitter.com/1.1/followers/ids.json'
+
 # Enedpointへ渡すパラメーター
-keyword = ["い min_retweets:50"]#20RT以上されてるツイートを取得
+keyword = ["竹内 min_retweets:500 " + "since:" + str(now_time.year) + '-' + str(now_time.month) + '-' + str(now_time.day - 1)] #20RT以上されてるツイートを取得
 params ={
-         'count' : 1000,      # 取得するtweet数
+         'count' : 1,      # 取得するtweet数
          'q'     :  keyword,# 検索キーワード
          'result_type': 'mixed',#時系列で取得
          'exclude': 'retweets'#RTされて表示されているツイートを除外
          }
 timeline_req = twitter.get(timeline_url, params = params)
-followers_req = twitter.get(follower_url, params = {'screen_name': '@iinoten'})
 if timeline_req.status_code == 200:
     res = json.loads(timeline_req.text)
     for line in res['statuses']:
-        if ( line["user"]["followers_count"] <= 500 ): #フォロワー数が500以下のツイートを表示 
-            print( line["text"] )
-            print( "https://twitter.com/" + line['user']['screen_name'] + "/status/" + line['id_str'] ) #ツイートのURLを表示
-            print('*******************************************')
-else:
-    print("Failed: %d" % timeline_req.status_code)
+        print(line["created_at"], line["id"], line["text"], line["retweet_count"], line["entities"]["urls"][0]["url"], line["user"]["verified"])
+        tweetId = line["id"]
+        reply = "悲しいですね"
+        mention = {"status":reply, "in_reply_to_status_id":tweetId, "auto_populate_reply_metadata":True}
+        resPost = twitter.post("https://api.twitter.com/1.1/statuses/update.json", params=mention)
+
+else: print("Failed: %d" % timeline_req.status_code)
