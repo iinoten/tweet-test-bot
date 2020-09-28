f962e1ad1ecfa9188d095f84108651375aedf978
ayamega <58317172+ayamega@users.noreply.github.com>
2020-09-28 16:53:59 +0900

    APIキーなどを隠した。リツイート数などで検索する機能を追加。

--- main.py
+++ main.py
@@ -1,40 +1,27 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-
 import json
-import config
 from requests_oauthlib import OAuth1Session
-import os
-from dotenv import load_dotenv
 
-load_dotenv()
-
-CK  = os.environ['CK']
-CS  = os.environ['CS']
-AT  = os.environ['AT']
-ATS = os.environ['ATS']
-
+CK  = 
+CS  = 
+AT  =
+ATS = 
 twitter = OAuth1Session(CK, CS, AT, ATS)
-
 # Twitter Endpoint(検索結果を取得する)
 timeline_url = 'https://api.twitter.com/1.1/search/tweets.json'
-
 #Twitter フォロワー数を取得する エンドポイント
 follower_url = 'https://api.twitter.com/1.1/followers/ids.json'
-
 # Enedpointへ渡すパラメーター
 keyword = ["い min_retweets:50"]#20RT以上されてるツイートを取得
-
 params ={
          'count' : 1000,      # 取得するtweet数
          'q'     :  keyword,# 検索キーワード
          'result_type': 'mixed',#時系列で取得
          'exclude': 'retweets'#RTされて表示されているツイートを除外
          }
-
 timeline_req = twitter.get(timeline_url, params = params)
 followers_req = twitter.get(follower_url, params = {'screen_name': '@iinoten'})
-
 if timeline_req.status_code == 200:
     res = json.loads(timeline_req.text)
     for line in res['statuses']:
