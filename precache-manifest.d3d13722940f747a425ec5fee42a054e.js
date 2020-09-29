4ef01f4bfac1c47d0d3ec9af512b5a835ef5ad51
Iino Ten <nteten0313@icloud.com>
2020-09-29 11:06:14 +0900

    Updates

---
+++ precache-manifest.d3d13722940f747a425ec5fee42a054e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e306f7fbaed48e7bc4e44cc114fa3581",
+    "url": "/tweet-test-bot/index.html"
+  },
+  {
+    "revision": "dd569d23856977e9149a",
+    "url": "/tweet-test-bot/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "3c897321c2dad2c96f4a",
+    "url": "/tweet-test-bot/static/js/2.c03f26a7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tweet-test-bot/static/js/2.c03f26a7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dd569d23856977e9149a",
+    "url": "/tweet-test-bot/static/js/main.0ca01317.chunk.js"
+  },
+  {
+    "revision": "d906d4b52b1980c314b3",
+    "url": "/tweet-test-bot/static/js/runtime-main.e0ba70c9.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/tweet-test-bot/static/media/logo.5d5d9eef.svg"
+  }
+]);
